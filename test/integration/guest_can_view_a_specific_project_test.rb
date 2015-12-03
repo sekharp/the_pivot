554dd8d35ba808d778d642d69976a238fe46e9fc
Pat Wey <patrickwey@gmail.com>
2015-12-03 10:53:49 -0700

    Add feature: Guest can view a specific project's info
    * Create link helper for Return to Projects link
    * Add classes to scope info on user/projects/show page
    * Move create_project and create_borrower to test_helper

---
+++ test/integration/guest_can_view_a_specific_project_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class GuestCanViewASpecificProjectTest < ActionDispatch::IntegrationTest
+  test "guest can view a specific project" do
+    borrower = create_borrower
+    borrower.projects << create_project
+
+    visit '/projects'
+    click_button 'Learn More'
+
+    assert_equal '/john-doe/projects/buy-me-a-goat', current_path
+
+    within '.project-info' do
+      assert page.has_content? 'Buy me a goat'
+      assert page.has_content? 'Mostly goat purchases'
+      assert page.has_content? '$1,000.00'
+    end
+
+    within '.borrower-info' do
+      assert page.has_content? 'John Doe'
+    end
+
+    within '.lend-btn' do
+      assert page.has_link? 'Lend'
+    end
+
+    assert page.has_link? 'Return to Projects'
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,4 +19,17 @@ class ActionDispatch::IntegrationTest
   def teardown
     reset_session!
   end
+
+  def create_borrower
+    User.create!(first_name: 'John',
+                 last_name:  'Doe',
+                 username:   'jdoe',
+                 password:   'password')
+  end
+
+  def create_project
+    Project.create!(goal_amount: 1000,
+                    title:       "Buy me a goat",
+                    description: "Mostly goat purchases")
+  end
 end

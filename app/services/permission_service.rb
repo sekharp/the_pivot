58d4bc6a70092f9c7a8fc81d44c149718870de70
Pat Wey <patrickwey@gmail.com>
2015-12-05 12:05:55 -0700

    Update feature to account for permissions
    * Add cart_project and cart permissions to guest
    * Add default of 0 to Project#total_loans

--- app/services/permission_service.rb
+++ app/services/permission_service.rb
@@ -49,6 +49,8 @@ class PermissionService
   def guest_permissions
     return true if controller == "session" && action.in?(%w(new create destroy))
     return true if controller == "users/projects" && action.in?(%w(index show))
+    return true if controller == "users/cart_projects" && action.in?(%w(new create))
+    return true if controller == "cart" && action == "index"
     return true if controller == "projects" && action == "index"
     return true if controller == "home" && action == "home"
   end

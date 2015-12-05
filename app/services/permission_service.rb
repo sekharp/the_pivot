a5ad56a94a546a638b9eca50e735cba067c6a923
Sekhar Paladugu <sekharp@gmail.com>
2015-12-05 11:41:39 -0700

    fix user projects controller and projects controller issues, close #24

--- app/services/permission_service.rb
+++ app/services/permission_service.rb
@@ -23,27 +23,34 @@ class PermissionService
 
   def platform_admin_permissions
     return true if controller == "session" && action.in?(%w(new create destroy))
-    return true if controller == "projects" && action.in?(%w(index show))
+    return true if controller == "users/projects" && action.in?(%w(index show))
+    return true if controller == "projects" && action == "index"
     return true if controller == "users" && action.in?(%w(index show))
     return true if controller == "loans" && action.in?(%w(index show))
+    return true if controller == "home" && action == "home"
   end
 
   def borrower_permissions
     return true if controller == "session" && action.in?(%w(new create destroy))
-    return true if controller == "projects" && action.in?(%w(index show))
+    return true if controller == "users/projects" && action.in?(%w(index show))
+    return true if controller == "projects" && action == "index"
     return true if controller == "loans" && action.in?(%w(index show))
+    return true if controller == "home" && action == "home"
   end
 
   def lender_permissions
     return true if controller == "session" && action.in?(%w(new create destroy))
-    return true if controller == "projects" && action.in?(%w(index show))
+    return true if controller == "users/projects" && action.in?(%w(index show))
+    return true if controller == "projects" && action == "index"
     return true if controller == "loans" && action == "index"
+    return true if controller == "home" && action == "home"
   end
 
   def guest_permissions
     return true if controller == "session" && action.in?(%w(new create destroy))
-    return true if controller == "home" && action == "home"
+    return true if controller == "users/projects" && action.in?(%w(index show))
     return true if controller == "projects" && action == "index"
+    return true if controller == "home" && action == "home"
   end
 
 end

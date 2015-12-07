9e59fa64b72c53e051a9d2bd860bcc0c7f4531b2
Pat Wey <patrickwey@gmail.com>
2015-12-06 17:57:06 -0700

    Close #33: Cart loan submission
    * Lenders can loan multiple borrowers projects at once
    * Add permission to lender for cart_projects#new

--- test/integration/lender_makes_loan_test.rb
+++ test/integration/lender_makes_loan_test.rb
@@ -25,4 +25,39 @@ class LenderMakesLoanTest < ActionDispatch::IntegrationTest
       assert page.has_content? 'Loan submitted'
     end
   end
+
+  test 'lender makes loan to multiple projects' do
+    create_roles
+    borrower = create_borrower
+    project = create_project
+    borrower.projects << project
+    add_project_to_cart(project)
+
+    borrower_b = User.create!(first_name: 'Chaim',
+                              last_name:  'Mejia',
+                              username:   'cmejia',
+                              password:   'password')
+    project_b = Project.create!(goal_amount: 5000,
+                                title:       "Buy me a boat",
+                                description: "I'm on a boat")
+    borrower_b.projects << project_b
+    add_project_to_cart(project_b)
+
+    lender = create_lender
+    login_lender
+
+    visit '/cart'
+    click_button 'Checkout'
+
+    assert_equal '/lender_dashboard', current_path
+    assert lender.loans.count == 2
+
+    within '#primary-navigation' do
+      assert page.has_content? '0'
+    end
+
+    within '#nav-bar' do
+      assert page.has_content? 'Loan submitted'
+    end
+  end
 end

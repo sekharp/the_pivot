a2aa4cec3c964963439f2b6e584fd0c9701593ed
Pat Wey <patrickwey@gmail.com>
2015-12-07 14:11:21 -0700

    Add cart and user model tests
    * Update methods on cart to refer to projects

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,40 +1,102 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  valid_params = { username: "beyonce",
-                   password: "putaringonit",
-                   role: 1 }
+  def valid_user
+    User.new({ username:   "pat",
+               password:   "password",
+               first_name: "pat",
+               last_name:  "wey"})
+  end
 
-  should have_many(:orders)
+  def create_role(name)
+    Role.create!(name: name)
+  end
 
-  test "it is valid with correct params" do
-    user = User.new(valid_params)
-    assert user.valid?
+  test "it is valid" do
+    assert valid_user.valid?
   end
 
   test "it is invalid without username" do
-    user = User.new( username: "",
-                     password: "password",
-                     role: 1 )
-    assert user.invalid?
+    user = valid_user
+    user.username = nil
+
+    refute user.valid?
   end
 
   test "it is invalid without password" do
-    user = User.new( username: "Dr. Dre",
-                     role: 1 )
-    assert user.invalid?
+    user = valid_user
+    user.password = nil
+
+    refute user.valid?
   end
 
-  test "username must be unique" do
-    user = User.create(valid_params)
-    same_user = User.new(valid_params)
+  test "it is invalid without first_name" do
+    user = valid_user
+    user.first_name = nil
 
-    assert same_user.invalid?
+    refute user.valid?
   end
 
-  test "has a default role as user" do
-    user = User.new( username: "will",
-                     password: "farrell")
-    assert_equal "default", user.role
+  test "it is invalid without last_name" do
+    user = valid_user
+    user.last_name = nil
+
+    refute user.valid?
   end
+
+  test "it must have a unique username" do
+    valid_user.save
+    same_user = User.new({username: "pat",
+                          password: "DIFFERENT",
+                          first_name: "DIFFERENT",
+                          last_name: "DIFFERENT"})
+
+    refute same_user.valid?
+  end
+
+  test "it responds to loans" do
+    assert valid_user.respond_to? :loans
+  end
+
+  test "it responds to projects" do
+    assert valid_user.respond_to? :projects
+  end
+
+  test "it responds to roles" do
+    assert valid_user.respond_to? :roles
+  end
+
+  test "it has a slug" do
+    assert_equal "pat-wey", valid_user.set_slug
+  end
+
+  test "it has a full name" do
+    assert_equal "pat wey", valid_user.full_name
+  end
+
+  test "it is an admin with an admin role" do
+    valid_user.save
+    user = User.first
+    user.roles << create_role("admin")
+
+    assert user.admin?
+  end
+
+  test "it is a borrower with a borrower role" do
+    valid_user.save
+    user = User.first
+    user.roles << create_role("borrower")
+
+    assert user.borrower?
+  end
+
+   test "it is a lender with an lender role" do
+    valid_user.save
+    user = User.first
+    user.roles << create_role("lender")
+
+    assert user.lender?
+  end
+
+
 end

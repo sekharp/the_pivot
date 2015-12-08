f769ac47dae39cc0a01c1ca61bc3d4915a4157ee
Matt Rooney <matthewjrooney@gmail.com>
2015-12-07 18:33:19 -0700

    update borrower dashboard and project creation - add default status to projects - update borrower dashboard with project details

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151206233147) do
+ActiveRecord::Schema.define(version: 20151208012450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,8 @@ ActiveRecord::Schema.define(version: 20151206233147) do
   create_table "projects", force: :cascade do |t|
     t.string   "title"
     t.string   "image_url"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
     t.integer  "category_id"
     t.text     "description"
     t.string   "image_file_name"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 20151206233147) do
     t.integer  "user_id"
     t.string   "slug"
     t.money    "goal_amount",        scale: 2
-    t.string   "status"
+    t.string   "status",                       default: "Pending"
   end
 
   add_index "projects", ["category_id"], name: "index_projects_on_category_id", using: :btree

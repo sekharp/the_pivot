23ff00ef82713f082e6407f36514e23e9032c8a7
Pat Wey <patrickwey@gmail.com>
2015-12-09 10:49:09 -0700

    Remove routes that aren't in controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
   root to: "home#home"
-  resources :borrower_projects, only: [:new, :create, :show]
+  resources :borrower_projects, only: [:new, :create]
 
   resources :projects, only: [:index, :show, :update]
   resources :categories, only: [:index, :show], param: :slug
-  resources :cart_projects, only: [:create, :update, :index, :destroy]
+  resources :cart_projects, only: [:create, :update, :destroy]
   resources :cart, only: [:index]
   resources :users, only: [:new, :create, :update, :edit]
   get "/lender_dashboard", to: "users#lender_dashboard"
   get "/borrower_dashboard", to: "users#borrower_dashboard"
-  get "/combined_dashboard", to: "users#combined_dashboard"
-  resources :lenders, only: [:new, :index, :show], param: :slug
-  resources :borrowers, only: [:new, :index, :show], param: :slug
+  resources :lenders, only: [:new], param: :slug
+  resources :borrowers, only: [:new], param: :slug
   get '/login', to: "session#new"
   post '/login', to: "session#create"
   delete '/logout', to: "session#destroy"

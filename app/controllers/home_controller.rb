8e02a4ec05455c20579ede2f175e94f09a8c1fe3
Matt Rooney <matthewjrooney@gmail.com>
2015-12-09 15:05:28 -0700

    initial commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def home
-    @top_projects = Project.where(status: "Active").limit(3)
+    @top_projects = top_projects
+  end
+
+  private
+
+  def top_projects
+    binding.pry
+    active = Project.where(status: "Active")
+    active.map { |pro| proj, proj.total_loans_amount }
   end
 end
